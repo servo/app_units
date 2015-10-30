ab15abf105136cbc0346b57dd293fe7345465ede
Manish Goregaokar <manishsmail@gmail.com>
2015-10-30 18:45:37 +0530

    Remove unconditional feature flags; works on stable

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![feature(custom_derive)]
-#![feature(plugin)]
-
+#![cfg_attr(feature = "plugins", feature(plugin))]
+#![cfg_attr(feature = "plugins", feature(custom_derive))]
 #![cfg_attr(feature = "plugins", plugin(serde_macros))]
 
 extern crate euclid;
